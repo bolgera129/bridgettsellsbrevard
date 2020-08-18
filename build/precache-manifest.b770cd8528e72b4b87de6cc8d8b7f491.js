5b42a3d17f71d2eb7f6378f7f25497b3b840bcdd
Anna Bolger <annabolger@Annas-MacBook-Pro.local>
2020-08-18 18:16:20 -0400

    homepage and build

---
+++ build/precache-manifest.b770cd8528e72b4b87de6cc8d8b7f491.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9f12a37c206f1719078ac954f94de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77916aaf62dd20818b6c",
+    "url": "/static/css/2.d7af4fa5.chunk.css"
+  },
+  {
+    "revision": "570577b614a8c853f77c",
+    "url": "/static/css/main.5432ab97.chunk.css"
+  },
+  {
+    "revision": "77916aaf62dd20818b6c",
+    "url": "/static/js/2.8a905189.chunk.js"
+  },
+  {
+    "revision": "2badc1b209c5a040c707dcd720b10f8a",
+    "url": "/static/js/2.8a905189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570577b614a8c853f77c",
+    "url": "/static/js/main.d702aac1.chunk.js"
+  },
+  {
+    "revision": "4080a94aff8c3dae7bf6",
+    "url": "/static/js/runtime-main.61b30640.js"
+  },
+  {
+    "revision": "95aed62101ca46f8d8196271f7d72319",
+    "url": "/static/media/Billy_Ohio.95aed621.ttf"
+  }
+]);
