b6295e7d4b36377e9d6c71854997cfaa07f8be3f
Anna Bolger <annabolger@Annas-MacBook-Pro.local>
2020-08-22 13:08:57 -0400

    fetch url

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import Home from "./Components/Home"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import SearchForm from 'Components/SearchForm';
 
 function App() {
-  require('dotenv').config()
   return (
-      <Router basename = {process.env.PUBLIC_URL}>
-          <main className = "marginBot20">
-            {
-              <Switch>
-                <Route path = "/home">
-                  <Home />
-                </Route>
-                {/* <Route path = "/resources">
-                  <Resources />
-                </Route> */}
-                <Route path = "/search">
-                  <SearchForm/>
-                </Route>
-                <Route path = ""> 
-                  <Home/>
-                </Route>
-              </Switch>
-            }        
-          </main>
-      </Router>   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/Utils/publicUrl.js
+++
@@ -1,5 +0,0 @@
-export default (assetPath)=>{
-        //return process.env.PUBLIC_URL+assetPath;
-    const publicUrl = assetPath.startsWith('data:image') ? '' : process.env.PUBLIC_URL;
-    return publicUrl + assetPath;
-}
