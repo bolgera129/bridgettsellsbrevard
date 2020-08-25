2232080de60e893c5610e39573f49b1ef7600b8e
Anna Bolger <annabolger@Annas-MacBook-Pro.local>
2020-08-25 10:55:18 -0400

    axios

--- functions/search.js
+++ functions/search.js
@@ -1,29 +1,29 @@
 var request = require("request");
+const axios = require('axios');
+
 
 exports.handler = async (event,context,callback) => {
+
   const body = JSON.parse(event.body)
-  console.log(body.url)
-    var options = {
-      method: 'GET',
-      uri: body.url,
-      headers: {
-        'x-rapidapi-host': 'realtor.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
-        useQueryString: true
+    axios({
+      "method":"GET",
+      "url":body.url,
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"realtor.p.rapidapi.com",
+      "x-rapidapi-key":process.env.REACT_APP_API_KEY,
+      "useQueryString":true
       }
-    };
-  request(options, function (error, response, body) {
-    result = JSON.parse(body)
-    console.log(result)
-    console.log(body)
-    callback(null, {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers':
-          'Origin, X-Requested-With, Content-Type, Accept'
-      },
-      body: JSON.stringify(result)
-    });
-  })
+    })
+    .then(
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers':
+            'Origin, X-Requested-With, Content-Type, Accept'
+        },
+        body: JSON.stringify(body)
+      })
+    )
 }
