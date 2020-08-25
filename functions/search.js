66489d138ce1274b9e73c6972018d6a1b9c25c61
Anna Bolger <annabolger@Annas-MacBook-Pro.local>
2020-08-25 09:52:47 -0400

    testing

--- functions/search.js
+++ functions/search.js
@@ -12,14 +12,7 @@ exports.handler = async (event,context,callback) => {
         useQueryString: true
       }
     };
-  var result;
   request(options, function (error, response, body) {
-    result = body;
-    console.log("result: " + result);
-    console.log("body: " + body);
     callback (null, {statusCode: 200, body: body})
   })
-  console.log("result 2: " + result)
-  callback (null, {statusCode: 200, body: result})
-
 }
